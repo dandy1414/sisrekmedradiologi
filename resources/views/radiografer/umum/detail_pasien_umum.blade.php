fc6a7b485d4d7245085e68054dabadff4d0e38bd
dandy1414 <dandyyusuf140897@gmail.com>
2020-08-25 18:13:40 +0700

    -fix icon on detail pasien

--- resources/views/radiografer/umum/detail_pasien_umum.blade.php
+++ resources/views/radiografer/umum/detail_pasien_umum.blade.php
@@ -25,37 +25,48 @@
                 <div class="box-body">
                     <div class="row">
                         <div class="col-md-7">
-                            <strong><i class="fa fa-map-marker margin-r-5"></i> Tanggal Pendaftaran :</strong>
+                            <strong><i class="fa fa-calendar"></i> Tanggal Pendaftaran :</strong>
+
                             <p class="text-muted">{{ $pasien->created_at->toDateString() }}</p>
 
-                            <strong><i class="fa fa-book margin-r-5"></i> Nomor Rekam Medis :</strong>
+                            <strong><i class="fa fa-medkit"></i> Nomor Rekam Medis :</strong>
+
                             <p class="text-muted">{{ $pasien->nomor_rm }}</p>
 
-                            <strong><i class="fa fa-book margin-r-5"></i> Nomor KTP :</strong>
+                            <strong><i class="fa fa-credit-card"></i> Nomor KTP :</strong>
+
                             <p class="text-muted">{{ $pasien->nomor_ktp }}</p>
 
-                            <strong><i class="fa fa-pencil margin-r-5"></i> Nama :</strong>
+                            <strong><i class="fa fa-user"></i> Nama :</strong>
+
                             <p class="text-muted">{{ $pasien->nama }}</p>
 
-                            <strong><i class="fa fa-pencil margin-r-5"></i> Jenis Kelamin :</strong>
+                            <strong><i class="fa fa-user"></i> Jenis Kelamin :</strong>
+
                             <p class="text-muted">{{ ucfirst($pasien->jenis_kelamin) }}</p>
 
-                            <strong><i class="fa fa-pencil margin-r-5"></i> Umur :</strong>
-                            <p class="text-muted">{{ $pasien->jenis_kelamin }} tahun</p>
+                            <strong><i class="fa fa-user"></i> Umur :</strong>
+
+                            <p class="text-muted">{{ $pasien->umur }} tahun</p>
                         </div>
 
                         <div class="col-md-5">
-                            <strong><i class="fa fa-pencil margin-r-5"></i> Alamat :</strong>
+                            <strong><i class="fa fa-home"></i> Alamat :</strong>
+
                             <p class="text-muted">{{ ucfirst($pasien->alamat) }}</p>
 
-                            <strong><i class="fa fa-pencil margin-r-5"></i> Nomor telepon :</strong>
+                            <strong><i class="fa fa-phone"></i> Nomor telepon :</strong>
+
                             <p class="text-muted">{{ $pasien->nomor_telepon }}</p>
 
-                            <strong><i class="fa fa-pencil margin-r-5"></i> Jenis Asuransi :</strong>
+                            <strong><i class="fa fa-institution"></i> Jenis Asuransi :</strong>
+
                             <p class="text-muted">{{ ucfirst($pasien->jenis_asuransi) }}</p>
 
-                            <strong><i class="fa fa-pencil margin-r-5"></i> Nomor BPJS :</strong>
-                            <p class="text-muted">{{ ($pasien->nomor_bpjs) != null ? $pasien->nomor_bpjs : "-" }}</p>
+                            <strong><i class="fa fa-bars"></i> Nomor BPJS :</strong>
+
+                            <p class="text-muted">
+                                {{ ($pasien->nomor_bpjs) != null ? $pasien->nomor_bpjs : "-" }}</p>
                         </div>
                     </div>
                     <!-- /.box-body -->
